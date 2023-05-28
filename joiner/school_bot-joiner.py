b5d4fb912cd93ee8b04bdd6275541630e734b20d
tipili-yusuf-ozer <96153804+tipili-yusuf-ozer@users.noreply.github.com>
2023-05-28 20:51:19 +0300

    AS

--- joiner/school_bot-joiner.py
+++ joiner/school_bot-joiner.py
@@ -12,6 +12,7 @@ from sys import argv, exit
 from time import sleep
 from re import compile, sub, IGNORECASE, search
 import datetime
+from csv import DictReader
 #selenium imports
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -19,10 +20,22 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import wait as waitt
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 #%% main
 def main():
     initialize()
-    login("402496","06428")
+    try:
+        with open("credentials.csv", "r") as f:
+           a=f.read().split(",")
+           credentials = {"username": a[0], "password": a[1]}
+    except FileNotFoundError:
+        username=input("Your first run, enter your credentials:\nusername: ")
+        password=input("\npassword: ")
+        credentials={"username": username, "password": password}
+        with open("credentials.csv", "w") as f:
+                f.write("{username},{password}".format(username=username, password=password))
+
+    login(credentials["username"],credentials["password"])
     lessons_tab=driver.find_element(by=By.XPATH, value="//div[@id='pc-for-in-progress']/div[@class='wdm-overview-slider m-0 row w-full slick-initialized slick-slider slick-dotted']")    
     nextbutton=lessons_tab.find_element(by=By.XPATH, value="button[2]")
     tabcursor=lessons_tab.find_element(by=By.XPATH, value="ul")
@@ -38,20 +51,26 @@ def main():
                 driver.get(lesson_page.get_dom_attribute('href'))
                 find_session(lesson_name)   
                 join_session()
+                with open("pipe/manager-joiner.txt", "w") as f:
+                    f.write("0")
 #%% wait untill everybody leaves
-                wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@id='roster-button']"))).click()
-                flag=0
-                while(1):
-                    participant_num=int(search(r"\d+",(wait.until(ec.visibility_of_element_located((By.XPATH, "//span[contains(@id, 'roster-title-section')]/span"))).text)).group())
-                    if participant_num<5:
-                        if flag==1:
-                            driver.quit()
-                            exit(0) #quit if no ones leftt in lesson
+                try:
+                    wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@id='roster-button']"))).click()
+                    flag=0
+                    while(1):
+                        global participant_num
+                        participant_num=int(search(r"\d+",(wait.until(ec.visibility_of_element_located((By.XPATH, "(//span[contains(@id, 'roster-title-section')])[2]/span"))).text)).group())
+                        if participant_num<5:
+                            if flag==1:
+                                driver.quit()
+                                exit(0) #quit if no ones leftt in lesson
+                            else:
+                                flag=1
                         else:
-                            flag=1
-                    else:
-                        flag=0
-                    sleep(600) #sleep 10 min and check again
+                            flag=0
+                        sleep(600) #sleep 10 min and check again
+                except TimeoutException:
+                    exit(3)
 #%% look for next section in lessons for the lesson
         wait.until(ec.element_to_be_clickable(nextbutton)).click()
         tabcursor_index+=1
@@ -75,7 +94,7 @@ def initialize():
     service=Service('driver/chromedriver.exe')
     driver = webdriver.Chrome(service=service,options=driveroption)
     logging.error("driverup %s", "",extra={'lesson': argv[1]})
-    wait=waitt.WebDriverWait(driver, 20) 
+    wait=waitt.WebDriverWait(driver, 25) 
     lesson_regex_text = sub('[si]', lambda x: {'s': '[sş]', 'i': '[iı]'}[x.group()], argv[1])
     lesson_regex=compile(f'.*{lesson_regex_text}.*',IGNORECASE)
     # lesson_regex=compile(r'.*introduction.*', IGNORECASE)
@@ -101,8 +120,8 @@ def find_session(lesson_name):
         driver.get(wait.until(ec.element_to_be_clickable((By.XPATH, f"//span[contains(text(), '{today}')]/.."))).get_attribute('href'))
         sessionlink=wait.until(ec.element_to_be_clickable((By.XPATH, "//a[contains(@href, 'https://teams.microsoft.com')]")))
     elif "Microprocessors" in lesson_name:
-        month, day=datetime.date(1,1,1).today.strftime('%B'), datetime.date(1,1,1).today.strftime('%-d')
-        lessonindex=datetime.date(1,1,1).today.isoweekday()-1 #1st lesson of the week or second
+        month, day=datetime.date(1,1,1).today().strftime('%B'), datetime.date(1,1,1).today().strftime('%d').lstrip('0')
+        lessonindex=datetime.date(1,1,1).today().isoweekday()-1 #1st lesson of the week or second
         driver.get(wait.until(ec.element_to_be_clickable((By.XPATH, f"//ul[@class='topics']//li[contains(@aria-label, '{month}') and contains(@aria-label, '{day}')]//li[contains(@class, 'activity page')][{lessonindex}]//div[@class='activityinstance']/a"))).get_attribute('href'))
         sessionlink=wait.until(ec.element_to_be_clickable((By.XPATH, "//a[contains(@href, 'https://teams.microsoft.com')]")))
     else:
@@ -128,6 +147,7 @@ def join_session():
 def hangup_and_quit(signum, frame):
         driver.find_element(By.XPATH, "//button[@id='hangup-button']").click()
         driver.quit()
+        exit(2)
 #%%run the code
 signal.signal(signal.SIGTERM, hangup_and_quit)
 logging.basicConfig(
@@ -137,16 +157,21 @@ logging.basicConfig(
     format="\n%(asctime)s:%(lesson)s:%(message)s",
     style="%",
     datefmt="%A %H.%M",
-    level=logging.ERROR
+    level=logging.INFO
     )
 try:
     main()
 except SystemExit as e:
-    if e.args[0]==1:
+    if e.args[0]==0:
+        logging.info("Successfully quit session with %s person left in session", participant_num, extra={"lesson": argv[1]})
+    elif e.args[0]==1:
         logging.error('No match found for the %s',argv[1], extra={"lesson": argv[1]})
-    elif e.args[0]==0:
-        logging.info('Program sucessfully quit with less than 5 persons left in lesson', extra={"lesson": argv[1]})
+    elif e.args[0]==2:
+        logging.info('Program quit the call before being terminated by unknown source', extra={"lesson": argv[1]})
+    elif e.args[0]==3:
+        logging.warning('Can\'t track listener count, program has to be terminated by manager')
 except:    
     driver.quit()
-    logging.error("Generic Error", exc_info=1, extra={"lesson": argv[1]})
-    raise
+    logging.error("Other Error", exc_info=True, extra={"lesson": argv[1]})
+    with open("pipe/manager-joiner.txt", "w") as f:
+        f.write("1")
